11d83ad43a9af640fb1b728aea815b1b221689cf
jjkazm <jjkazm@gmail.com>
2018-09-28 00:21:48 +0200

    add task controller and views

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
+  def index
+    @done = Task.where(complete: true)
+    @todo = Task.where(complete: false)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create!(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :complete)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+    def set_article
+    @article = Article.find(params[:id])
+  end
+end
